b8a970c9b1c7906611706f311eb803da21f531f2
Chareeef <youssef.charif.h@gmail.com>
2024-05-09 19:34:33 +0000

    Implement CRUD Ops with redis

---
+++ test.js
@@ -0,0 +1,19 @@
+import redisClient from './utils/redis';
+
+(async () => {
+    console.log(true, redisClient.isAlive());
+    console.log(null, await redisClient.get('myKey'));
+    await redisClient.set('myKey', 12, 3);
+    console.log(12, await redisClient.get('myKey'));
+
+    await redisClient.set('key', 89, 777);
+    console.log(89, await redisClient.get('key'));
+    await redisClient.del('key');
+    console.log(null, await redisClient.get('key'));
+    console.log('Error:');
+    await redisClient.del('key');
+
+    setTimeout(async () => {
+        console.log(null, await redisClient.get('myKey'));
+    }, 1000*4)
+})();
--- utils/redis.js
+++ utils/redis.js
@@ -11,7 +11,40 @@ class RedisClient {
       .on('connect', () => (this.isConnected = true));
   }
 
+  // Check if the redis client is connected
   isAlive() {
     return this.isConnected;
   }
+
+  // Get a key's value
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+
+    try {
+      return await getAsync(key);
+    } catch (err) {
+      return null
+    }
+  }
+
+  // Set a key-value pair with expiration time
+  async set(key, value, ex_time) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    await setAsync(key, ex_time, value);
+  }
+
+  // Delete a key
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+
+    try {
+      await delAsync(key);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 }
+
+// Export a RedisClient instance
+const redisClient = new RedisClient();
+export default redisClient;
