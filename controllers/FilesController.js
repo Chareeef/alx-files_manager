f449a683c527a425a8aa40cc3bf2de6b51af67ac
Chareeef <youssef.charif.h@gmail.com>
2024-05-10 19:09:58 +0000

    Collect data about the file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -16,4 +16,37 @@ export async function postUpload(req, res) {
   if (!user) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
+
+  // Retrieve tha file's informations from req.body and handlr any missing one
+  const name = req.body.name;
+  if (!name) {
+    return res.status(400).json({ error: 'Missing name' });
+  }
+
+  const type = req.body.type;
+  if (!type || !(['folder', 'file', 'image'].includes(type]))) {
+    return res.status(400).json({ error: 'Missing type' });
+  }
+
+  const data = req.body.data;
+  if (!data && type !== 'folder') {
+    return res.status(400).json({ error: 'Missing data' });
+  }
+
+  const parentId = req.body.parentId ? req.body.parentId : 0;
+  if (parentId !== 0) {
+
+    // Check if the parent folder exists
+    const parentFolder = dbClient.findOne('files', { _id: new ObjectId(parentId) });
+    if (!parentFolder) {
+      return res.status(400).json({ error: 'Parent not found' });
+    }
+
+    // Ensure it is a folder
+    if (parentFolder.type !== 'folder') {
+      return res.status(400).json({ error: 'Parent is not a folder' });
+    }
+  }
+
+  const isPublic = req.body.isPublic ? req.body.isPublic : false;
 }
